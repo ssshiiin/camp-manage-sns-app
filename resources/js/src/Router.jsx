0413a92d32a3a4f1a3fe892824fc30de7ec123c1
Takumi SHINGAI <s1f101902433@iniad.org>
2021-11-24 11:10:31 +0900

    routing fix

--- resources/js/src/Router.jsx
+++ resources/js/src/Router.jsx
@@ -3,13 +3,16 @@ import { Route, Switch } from 'react-router';
 import { Home, Schedule } from './templates/index';
 import RouterProfile from './RouterProfile';
 import RouterGear from './RouterGear';
-import RouterAuth from './RouterAuth';
+import { PasswordReset, PasswordResetToken, Register, SignIn } from './templates/Auth';
 
 const Router = () => {
   return (
     <>
-      <RouterAuth />
       <Switch>
+        <Route path="/login" exact component={SignIn} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/password/reset" exact component={PasswordReset} />
+        <Route path="/password/reset/:token" exact component={PasswordResetToken} />
         <Route path="/" exact component={Home} />
         <Route path="/site" exact component={Schedule} />
         <Route path="/:id/bring" component={RouterGear} />
